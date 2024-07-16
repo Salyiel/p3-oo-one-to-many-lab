20b25c4a28812a38bba2e1d638758367515539ad
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-16 12:34:43 +0300

    Owner class code

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,4 +1,19 @@
 class Pet:
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+             return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be an instance of the Pet class.")
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
     pass
 
 class Owner:
